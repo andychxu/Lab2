fa91f0dbdfe027c5516c4478770dd41fde345919
andychxu <andy.25@ichat.sp.edu.sg>
2026-04-28 14:32:54 +0800

    lab 2 first edit

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,29 @@
-print("ET0735 (DevOps for AIOT) - Lab 2 - Introduction to Python")
+# print("ET0735 (DevOps for AIOT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    print("get_user_input")
+
+def calc_average():
+    print("calc_average")
+
+def find_min_max():
+    print("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+
+def main():
+    print("ET0735 Lab 2")
+    display_main_menu()
+    get_user_input()
+
+
+if __name__ == "__main__":
+    main()
