c3d7798087f038bf9e4106e38da4905224c1648b
andychxu <andy.25@ichat.sp.edu.sg>
2026-04-28 15:03:54 +0800

    done with exercise 5

--- Lab2.py
+++ Lab2.py
@@ -1,16 +1,27 @@
 # print("ET0735 (DevOps for AIOT) - Lab 2 - Introduction to Python")
 
 def display_main_menu():
-    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 
 def get_user_input():
-    print("get_user_input")
+    user_input = input()
 
-def calc_average():
-    print("calc_average")
+    str_list = user_input.split(",")
 
-def find_min_max():
-    print("find_min_max")
+    num_list = []
+    for item in str_list:
+        num_list.append(float(item))
+
+    return num_list
+
+def calc_average_temperature(num_list):
+    average = sum(num_list) / len(num_list)
+    return average
+
+def calc_min_max_temperature(num_list):
+    min_temp = min(num_list)
+    max_temp = max(num_list)
+    return [min_temp, max_temp]
 
 def sort_temperature():
     print("sort_temperature")
@@ -18,11 +29,18 @@ def sort_temperature():
 def calc_median_temperature():
     print("calc_median_temperature")
 
-
 def main():
     print("ET0735 Lab 2")
     display_main_menu()
-    get_user_input()
+    num_list = get_user_input()
+    print(num_list)
+
+    average = calc_average_temperature(num_list)
+    print("Average temperature = " + str(average))
+
+    min_max = calc_min_max_temperature(num_list)
+    print("Minimum temperature = " + str(min_max[0]))
+    print("Maximum temperature = " + str(min_max[1]))
 
 
 if __name__ == "__main__":
