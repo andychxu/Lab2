84d139921508949445886fe37c5464cffc8f1e19
andychxu <andy.25@ichat.sp.edu.sg>
2026-05-05 10:46:32 +0800

    updated bmi to return -1,0,1

--- bmi.py
+++ bmi.py
@@ -7,9 +7,12 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Under Weight")
+        return -1
     elif bmi <= 25:
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
